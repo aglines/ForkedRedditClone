6aa73d2cd04d289565a9673819a162eb25905ca5
daniel lopez and joshua fairchild <daniel+joshua@>
2017-07-10 20:00:50 -0700

    refactor a few service methods to be much more succinct

--- src/app/subreddit.service.ts
+++ src/app/subreddit.service.ts
@@ -21,23 +21,31 @@ export class SubredditService {
   }
 
   getPostById(id: number): UserPost {
-    for(let i = 0; i < SUBREDDITS.length; i++) {
-      for(let j = 0; j < SUBREDDITS[i].userPosts.length; j++) {
-        if (SUBREDDITS[i].userPosts[j].id === id) {
-          return SUBREDDITS[i].userPosts[j]
-        }
-      }
-    }
+    // for(let i = 0; i < SUBREDDITS.length; i++) {
+    //   for(let j = 0; j < SUBREDDITS[i].userPosts.length; j++) {
+    //     if (SUBREDDITS[i].userPosts[j].id === id) {
+    //       return SUBREDDITS[i].userPosts[j]
+    //     }
+    //   }
+    // }
+    //
+    // better version below
+
+    return this.getAllPosts().filter(post => post.id === id)[0];
   }
 
   getSubredditTitleByPostID(id: number): string {
-    for(let i = 0; i < SUBREDDITS.length; i++) {
-      for(let j = 0; j < SUBREDDITS[i].userPosts.length; j++) {
-        if (SUBREDDITS[i].userPosts[j].id === id) {
-          return SUBREDDITS[i].title;
-        }
-      }
-    }
+    // for(let i = 0; i < SUBREDDITS.length; i++) {
+    //   for(let j = 0; j < SUBREDDITS[i].userPosts.length; j++) {
+    //     if (SUBREDDITS[i].userPosts[j].id === id) {
+    //       return SUBREDDITS[i].title;
+    //     }
+    //   }
+    // }
+    //
+    // better version below
+
+    return SUBREDDITS.filter(sub => sub.userPosts.filter(post => post.id === id).length === 1)[0].title
   }
 
   addComment(commentToAdd: string, targetPostId: number): void {
@@ -51,24 +59,29 @@ export class SubredditService {
   }
 
   generatePostId(): number {
-    let generatedId = 0;
-    let posts = this.getAllPosts();
+    const posts = this.getAllPosts();
+    // let generatedId = 0;
+    //
+    // for(let i = 0; i < posts.length; i++) {
+    //   if (posts[i].id > generatedId) {
+    //     generatedId = posts[i].id;
+    //   }
+    // }
 
-    for(let i = 0; i < posts.length; i++) {
-      if (posts[i].id > generatedId) {
-        generatedId = posts[i].id;
-      }
-    }
-    return generatedId + 1;
+    // return generatedId + 1;
+
+    return posts[posts.length - 1].id + 1
   }
 
-  getAllPosts() {
-    let output = [];
+  getAllPosts(): UserPost[] {
+    // let output = [];
+    //
+    // SUBREDDITS.forEach(sub => {
+    //   output.push(...sub.userPosts);
+    // });
+    //
+    // return output;
 
-    SUBREDDITS.forEach(sub => {
-      output.push(...sub.userPosts);
-    });
-
-    return output;
+    return SUBREDDITS.reduce((prev, curr) => [...prev, ...curr.userPosts], [])
   }
 }
